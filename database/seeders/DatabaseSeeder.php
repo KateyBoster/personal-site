f96288f328e61b63ff88c557a608f64ac303bd02
Katey Boster <katey.boster@gmail.com>
2025-04-27 19:06:29 -0400

    updated composer, layout and db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +18,13 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('TestPassword123')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Katey Boster',
+            'email' => 'katey.boster@gmail.com',
+            'password' => Hash::make('TestPassword123')
         ]);
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import {
     defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+} from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
